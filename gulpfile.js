dc4a352480f6430fcaa29a4dde85a807c7c9d671
ivpopov <iv.popov@estp.ru>
2015-07-10 16:29:46 +0300

    Добавил плагин gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 var gulp = require('gulp'),
     jscs = require('gulp-jscs'),
     concat = require('gulp-concat'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    uglify = require('gulp-uglify');
 
 gulp.task('scripts', function () {
     return gulp.src(['src/**/*.js'])
         .pipe(plumber())
         .pipe(jscs())
-        .pipe(concat('dest.js'))
-        .pipe(gulp.dest('build'))
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('watch', function () {
-   gulp.watch('src/**', ['scripts'])
+    gulp.watch('src/**', ['scripts']);
 });
 
 gulp.task('default', ['watch']);
